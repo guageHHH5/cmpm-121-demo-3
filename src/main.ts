822675881a1fd9ddad7d97657308550a72bda63a
Eric Wang <twang173@ucsc.edu>
2024-11-08 14:30:13 -0800

    dialogue complete

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
 // todo
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.getElementById("button");
+
+  if (button) {
+    button.addEventListener("click", () => {
+      alert("You clicked the button!");
+    });
+  } else {
+    console.error("Button not found!");
+  }
+});
