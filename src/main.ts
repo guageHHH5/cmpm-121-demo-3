6aead2c84fcf0a40b96b0aa22fa9a660fbb36166
Eric Wang <twang173@ucsc.edu>
2024-11-08 14:33:18 -0800

    dialogue revision

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 // todo
 document.addEventListener("DOMContentLoaded", () => {
-  const button = document.getElementById("button");
+  const button = document.getElementById("clickbutton");
 
   if (button) {
     button.addEventListener("click", () => {
       alert("You clicked the button!");
     });
   } else {
-    console.error("Button not found!");
+    console.error("No Button Found!");
   }
 });
