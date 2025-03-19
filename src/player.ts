85bde4a2799aff10defdee90894b240bbe81fb42
guageHHH5 <71291527+guageHHH5@users.noreply.github.com>
2025-03-19 02:58:03 -0700

    added function for map following player movement

--- src/player.ts
+++ src/player.ts
@@ -6,7 +6,10 @@ export class Player{
     private readonly coins: Coin[];
     private location: leaflet.LatLng;
     private marker: leaflet.Marker;
+    private observer: (()=>void)[] = [];
   
+    private MOVE_OFFEST = 0.00001;
+
     constructor(location: leaflet.LatLng, map: leaflet.Map){
       this.coins = [];
       this.location = location;
@@ -33,5 +36,41 @@ export class Player{
   
     public getCoinCount(): number{
       return this.coins.length;
+    }
+
+    public moveUp(): void{
+      const lat = this.location.lat + this.MOVE_OFFEST;
+      this.move(lat, this.location.lng);
+    }
+
+    public moveDown(): void{
+      const lat = this.location.lat - this.MOVE_OFFEST;
+      this.move(lat, this.location.lng);
+    }
+
+    public moveLeft(): void{
+      const lng = this.location.lng - this.MOVE_OFFEST;
+      this.move(this.location.lat, lng);
+    }
+
+    public moveRight(): void{
+      const lng = this.location.lng + this.MOVE_OFFEST;
+      this.move(this.location.lat, lng);
+    }
+
+    public move(lat: number, lng: number): void {
+      this.location = leaflet.latLng(lat, lng);
+      this.marker.setLatLng(this.location);
+      this.notifyObserver();
+    }
+
+    public createObserver(observers: () => void): void {
+      this.observer.push(observers);
+    }
+
+    public notifyObserver(): void {
+      for (const observers of this.observer){
+        observers();
+      }
     }
   }
