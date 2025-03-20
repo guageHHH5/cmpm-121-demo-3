f020d7913cf90c0effd50599f1260c4fb220e32d
guageHHH5 <71291527+guageHHH5@users.noreply.github.com>
2025-03-19 17:57:24 -0700

    changed movement function
    now player move based on lat and lng offsets.
    also added serialization and deserialization.

--- src/player.ts
+++ src/player.ts
@@ -7,7 +7,8 @@ export class Player{
     private location: leaflet.LatLng;
     private marker: leaflet.Marker;
     private observer: (()=>void)[] = [];
-  
+
+    private movementHistory: leaflet.LatLng[] = [];
     private MOVE_OFFEST = 0.00001;
 
     constructor(location: leaflet.LatLng, map: leaflet.Map){
@@ -21,11 +22,36 @@ export class Player{
     public getLocation(): leaflet.LatLng{
       return this.location;
     }
-  
+
+    public getCoinInventory(){
+      return this.coins;
+    }
+
+    public getPlayer(): leaflet.Marker{
+      return this.marker;
+    }
+
+    public getMovementHistory(): leaflet.LatLng[]{
+      return this.movementHistory;
+    }
+
+    public getPlayerLocation(): leaflet.LatLng{
+      return this.location;
+    }
+    
     public setLocation(location:leaflet.LatLng): void{
       this.location = location;
+      this.marker.setLatLng(this.location);
+      this.notifyObserver();
     }
-  
+
+    public moveByLatLng(latOffset: number, lngOffset: number): void{
+      const nLat = this.location.lat + latOffset;
+      const nLng = this.location.lng + lngOffset;
+      const nLoc = leaflet.latLng(nLat, nLng);
+      this.setLocation(nLoc);    
+    }
+    
     public addCoin(coin: Coin): void{
       this.coins.push(coin);
     }
@@ -39,30 +65,32 @@ export class Player{
     }
 
     public moveUp(): void{
-      const lat = this.location.lat + this.MOVE_OFFEST;
-      this.move(lat, this.location.lng);
+      //const lat = this.location.lat + this.MOVE_OFFEST;
+      this.moveByLatLng(this.MOVE_OFFEST, 0);
     }
 
     public moveDown(): void{
-      const lat = this.location.lat - this.MOVE_OFFEST;
-      this.move(lat, this.location.lng);
+      //const lat = this.location.lat - this.MOVE_OFFEST;
+      this.moveByLatLng(-this.MOVE_OFFEST, 0);
     }
 
     public moveLeft(): void{
-      const lng = this.location.lng - this.MOVE_OFFEST;
-      this.move(this.location.lat, lng);
+      //const lng = this.location.lng - this.MOVE_OFFEST;
+      this.moveByLatLng(0, -this.MOVE_OFFEST);
     }
 
     public moveRight(): void{
-      const lng = this.location.lng + this.MOVE_OFFEST;
-      this.move(this.location.lat, lng);
+      //const lng = this.location.lng + this.MOVE_OFFEST;
+      this.moveByLatLng(0, this.MOVE_OFFEST);
     }
 
+    /*
     public move(lat: number, lng: number): void {
       this.location = leaflet.latLng(lat, lng);
       this.marker.setLatLng(this.location);
       this.notifyObserver();
     }
+      */
 
     public createObserver(observers: () => void): void {
       this.observer.push(observers);
@@ -72,5 +100,33 @@ export class Player{
       for (const observers of this.observer){
         observers();
       }
+    }
+
+    public resetCoins(): void {
+      this.coins.length = 0;
+    }
+
+    serializePlayer(): string {
+      return JSON.stringify({
+        location: this.location,
+        coins: this.coins.map((coin) => ({
+          spawnLoc: coin.spawnLocation,
+          serial: coin.serial,
+        })),
+      });
+    }
+
+    static deserializePlayer(data: string, map: leaflet.Map): Player {
+      console.log("Deserializing player...");
+      const parsed = JSON.parse(data);
+      console.log("object returned by JSON parse: ", parsed);
+      const player = new Player(
+        leaflet.latLng(parsed.location.lat, parsed.location.lng),
+        map,
+      );
+      parsed.coins.forEach((coin: Coin) => {
+        player.addCoin(new Coin(coin.spawnLocation, coin.serial));
+      });
+      return player;
     }
   }
